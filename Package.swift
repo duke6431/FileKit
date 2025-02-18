e6e579a74343280baac9d5e0ed6929b20eee077a
DukeNguyen <duke@castalk.com>
2025-02-18 13:22:50 +0700

    Change: - Add Dynamic library

--- Package.swift
+++ Package.swift
@@ -32,9 +32,8 @@ let package = Package(
     name: "FileKit",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "FileKit",
-            targets: ["FileKit"]),
+        .library(name: "FileKit", targets: ["FileKit"]),
+        .library(name: "FileKit-Dynamic", type: .dynamic, targets: ["FileKit"]),
     ],
     dependencies: [],
     targets: [
